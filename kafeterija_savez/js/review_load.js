2bf6c7079a331ffd59b4a6683dae5c0d0a756b1d
Marko Gudžev <57346877+gudzev@users.noreply.github.com>
2024-10-22 17:39:36 +0200

    Add files via upload

---
+++ kafeterija_savez/js/review_load.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const animatedElements = document.querySelectorAll('.review, .kriterijumi-h1, .parametri-text, .btn-search, #ocena');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                // Add the animation class when the element comes into view
+                entry.target.classList.add('animate-fadein');
+                // Optionally, you can unobserve the element after adding the animation class
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.1 }); // Trigger when 10% of the element is in view
+
+    animatedElements.forEach(el => {
+        observer.observe(el);
+    });
+});
+
+if ( window.history.replaceState ) 
+    {
+    window.history.replaceState( null, null, window.location.href );
+  }
